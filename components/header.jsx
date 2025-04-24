51e28bad2779d3075a9b383e4780cfb10a382437
heikkuj <heikkuamo@gmail.com>
2025-04-24 21:48:49 +0200

    new header padding

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className="flex mt-3 p-3 bg-primary-1 w-[90vw] justify-self-end rounded-l-lg">
+    <div className="flex mt-3 py-3 px-6 bg-primary-1 w-[90vw] justify-self-end rounded-l-lg">
         <p className="text-5xl font-barcode text-black">Heikku Jensen</p>
     </div>
   )
