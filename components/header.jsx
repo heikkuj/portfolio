c60ec3dd2065fda215223a7af35f68bee147851b
heikkuj <heikkuamo@gmail.com>
2025-04-28 10:31:40 +0200

    link for eng added

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
-    <div className="flex mt-3 py-3 px-6 bg-primary-1 w-[90vw] justify-self-end rounded-l-lg">
+    <div className="flex mt-3 py-3 px-6 bg-primary-1 w-[90vw] justify-between justify-self-end rounded-l-lg">
         <p className="text-5xl font-barcode text-black">Heikku Jensen</p>
+        <Link href='/'><p>ENG</p></Link>
     </div>
   )
 }
