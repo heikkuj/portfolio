cb1bc5ddcf4f2dcec299274e52614ef670bd9973
heikkuj <heikkuamo@gmail.com>
2025-05-08 15:57:43 +0200

    minor css changes to container title

--- components/container-right.jsx
+++ components/container-right.jsx
@@ -4,9 +4,9 @@ export default function ContainerR({children, title}) {
   return (
     <div className="flex flex-col mt-2 p-4 w-[85vw] h-auto justify-self-end bg-primary-2 rounded-l-lg">
 
-        <h1 className="font-light text-lg text-white place-self-end">{title}</h1>
+        <h1 className="font-semibold text-lg uppercase tracking-wider text-white place-self-start underline decoration-2 decoration-solid underline-offset-5">{title}</h1>
         <div className='text-right'>
-        <div className='my-2 place-self-end h-[1px] w-[50%] bg-amber-800' />
+          {/* <div className='my-2 place-self-start w-[40%] md:w-[30%] lg:w-[30%] border-1 border-bg-amber-800' /> */}
         {children}
         </div>
     </div>
