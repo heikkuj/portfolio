049ac6e93045f635ac8d68a16cd7d596762a3ddb
heikkuj <heikkuamo@gmail.com>
2025-05-24 14:54:17 +0200

    updated color palette for progress bars

--- components/progress-bar.jsx
+++ components/progress-bar.jsx
@@ -5,11 +5,11 @@ export function ProgNovice({ title }) {
     <div className="flex flex-col font-semibold mx-3 my-3 place-items-center">
         <h2>{title}</h2>
 
-        <div className='flex mt-1 bg-primary-3 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2'>
-            <div className="w-[25%] border-r-1 bg-primary-1" />
-            <div className="w-[25%] border-x-1 " />
-            <div className="w-[25%] border-x-1" />
-            <div className="w-[25%] border-l-1" />
+        <div className='flex mt-1 bg-accent-1 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2 border-progress-border'>
+            <div className="w-[25%] border-r-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 border-progress-border" />
+            <div className="w-[25%] border-l-1 border-progress-border" />
         </div>
         <p className="m-1 text-sm">Nybegynner</p>
     </div>
@@ -21,11 +21,11 @@ export function ProgIntermediate({ title }) {
     <div className="flex flex-col font-semibold mx-3 my-3 place-items-center">
         <h2>{title}</h2>
 
-        <div className='flex mt-1 bg-primary-3 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2'>
-            <div className="w-[25%] border-r-1 bg-primary-1" />
-            <div className="w-[25%] border-x-1 bg-primary-1" />
-            <div className="w-[25%] border-x-1" />
-            <div className="w-[25%] border-l-1" />
+        <div className='flex mt-1 bg-accent-1 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2 border-progress-border'>
+            <div className="w-[25%] border-r-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 border-progress-border" />
+            <div className="w-[25%] border-l-1 border-progress-border" />
         </div>
         <p className="m-1 text-sm">Viderekommen</p>
     </div>
@@ -37,11 +37,11 @@ export function ProgAdvanced({ title }) {
     <div className="flex flex-col font-semibold mx-3 my-3 place-items-center">
         <h2>{title}</h2>
 
-        <div className='flex mt-1 bg-primary-3 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2'>
-            <div className="w-[25%] border-r-1 bg-primary-1" />
-            <div className="w-[25%] border-r-1 bg-primary-1" />
-            <div className="w-[25%] border-r-1 bg-primary-1" />
-            <div className="w-[25%] border-l-1" />
+        <div className='flex mt-1 bg-accent-1 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2 border-progress-border'>
+            <div className="w-[25%] border-r-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-r-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-r-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-l-1 border-progress-border" />
         </div>
         <p className="m-1 text-sm">Avansert</p>
     </div>
@@ -53,11 +53,11 @@ export function ProgPro({ title }) {
     <div className="flex flex-col font-semibold mx-3 my-3 place-items-center text-center">
         <h2>{title}</h2>
 
-        <div className='flex mt-1 bg-primary-3 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2'>
-            <div className="w-[25%] border-r-1 bg-primary-1" />
-            <div className="w-[25%] border-x-1 bg-primary-1" />
-            <div className="w-[25%] border-x-1 bg-primary-1" />
-            <div className="w-[25%] border-x-1 bg-primary-1" />
+        <div className='flex mt-1 bg-accent-1 rounded-full h-[15px] lg:h-[20px] overflow-hidden w-[100%] max-w-[400px] lg:max-w-[700px] md:max-w-[700px] border-2 border-progress-border'>
+            <div className="w-[25%] border-r-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 bg-primary-1 border-progress-border" />
+            <div className="w-[25%] border-x-1 bg-primary-1 border-progress-border" />
         </div>
         <p className="m-1 text-sm">Ekspert</p>
     </div>
